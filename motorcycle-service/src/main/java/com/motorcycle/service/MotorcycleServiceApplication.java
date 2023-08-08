b2057d13c6639f76963c06ec77e9670705ed10d9
HarDep <haroldralvaradol@gmail.com>
2023-08-08 14:01:13 -0500

    eureka

--- motorcycle-service/src/main/java/com/motorcycle/service/MotorcycleServiceApplication.java
+++ motorcycle-service/src/main/java/com/motorcycle/service/MotorcycleServiceApplication.java
@@ -2,8 +2,10 @@ package com.motorcycle.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MotorcycleServiceApplication {
 
 	public static void main(String[] args) {
